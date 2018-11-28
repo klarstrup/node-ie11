d4dcc8bd6364bab29c52cb3e92fe87707035cc85
klarstrup <jonas@klarstrup.dk>
2018-11-28 03:36:42 +0100

    good shit

--- cli.js
+++ cli.js
@@ -1,53 +1,117 @@
 #!/usr/bin/env node
 
 const rollup = require("rollup");
+const babelParser = require("@babel/parser");
 const resolve = require("rollup-plugin-node-resolve");
 const babel = require("rollup-plugin-babel");
-const [input] = process.argv.slice(2);
+const prettier = require("rollup-plugin-prettier");
+const commonjs = require("rollup-plugin-commonjs");
+const fs = require("fs");
+const program = require("commander");
 
-if (!input) {
-  console.error(
-    "First argument to `ie11` must be the path to a .js script file."
-  );
-  process.exit();
-}
-const output = input
-  .split(".")
-  .map(s => (s === "js" ? "ie11.js" : s))
-  .join(".");
-rollup
-  .rollup({
-    input,
-    plugins: [
-      resolve(),
-      babel({
-        babelrc: false,
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              targets: {
-                browsers: ["ie 11"]
-              },
-              modules: false,
-              useBuiltIns: "usage"
-            }
-          ]
+program
+  .version("0.1.0")
+  .option("-o, --outputFilename")
+  .action((inputFilename, outputFilename) => {
+    if (!inputFilename) {
+      console.error(
+        "First argument to `ie11` must be the path to a .js script file."
+      );
+      process.exit();
+    }
+
+    rollup
+      .rollup({
+        input: inputFilename,
+        treeshake: true,
+        plugins: [
+          codeMangler(),
+          resolve(),
+          commonjs(),
+          babel({
+            babelrc: false,
+            exclude: "node_modules/**", // only transpile our source code
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    browsers: ["ie 11"]
+                  },
+                  modules: false,
+                  useBuiltIns: "usage"
+                }
+              ]
+            ]
+          }),
+          prettier({ parser: "babylon" })
         ]
       })
-    ]
+      .then(bundle => {
+        const outputOptions = {
+          format: "iife",
+          strict: false,
+          indent: false
+        };
+        if (typeof outputFilename === "string") {
+          bundle
+            .write({
+              ...outputOptions,
+              file: outputFilename
+            })
+            .then(() =>
+              console.log(
+                "Transmogriffed %s into the IE11-friendly %s 🤠",
+                inputFilename,
+                outputFilename
+              )
+            );
+        } else {
+          bundle
+            .generate({
+              ...outputOptions
+            })
+            .then(({ code }) => console.log(code));
+        }
+      });
   })
-  .then(bundle =>
-    bundle
-      .write({
-        file: output,
-        format: "iife"
-      })
-      .then(() =>
-        console.log(
-          "Transmogriffed %s into the IE11-friendly %s 🤠",
-          input,
-          output
-        )
-      )
-  );
+  .parse(process.argv);
+
+let topLevelDeclarations;
+function codeMangler() {
+  return {
+    name: "my-example", // this name will show up in warnings and errors
+    resolveId(importee, importer) {
+      if (importee === "virtual-module") {
+        return importee; // this signals that rollup should not ask other plugins or check the file system to find this id
+      }
+      return null; // other ids should be handled as usually
+    },
+    generateBundle(outputOptions, bundle, isWrite) {
+      // Strip out IIFE and any window. trickery
+      /*
+      for (let part in bundle) {
+        bundle[part].code = bundle[part].code
+          .split("\n")
+          .slice(1, -(2 + topLevelDeclarations.length))
+          .join("\n");
+      }
+      */
+    },
+    transform(code, id) {
+      if (!topLevelDeclarations) {
+        topLevelDeclarations = babelParser
+          .parse(code)
+          .program.body.map(({ id: { name } = {} }) => name)
+          .filter(Boolean);
+        const newCode = `
+          ${code}
+          ${topLevelDeclarations
+            .map(identifier => `window.${identifier} = ${identifier};`)
+            .join("\n")}
+        `;
+        return newCode; // the source code for "virtual-module"
+      }
+    }
+  };
+}
